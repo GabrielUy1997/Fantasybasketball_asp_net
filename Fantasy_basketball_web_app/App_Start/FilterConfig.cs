1ca7ce612048311a143a5484a0d51326a2579eee
gabriel uy <uy.gabriel1997@gmail.com>
2020-11-11 15:48:32 -0500

    Add project files.

---
+++ Fantasy_basketball_web_app/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Fantasy_basketball_web_app
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Fantasy_basketball_web_app/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fantasy_basketball_web_app.MvcApplication" Language="C#" %>
